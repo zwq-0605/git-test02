26fd1f53f38ddd431d907da79e2df8f9dd47af6b
zwq <1783120538@qq.com>
2021-09-03 03:46:26 +0000

    update src/main/java/com/zou/git/GitTest.java.

--- src/main/java/com/zou/git/GitTest.java
+++ src/main/java/com/zou/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("push gitee test");
+        System.out.println("pull gitee test");
     }
 }
